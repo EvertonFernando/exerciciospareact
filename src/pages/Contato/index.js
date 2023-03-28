08e5a9077ae3ac08bc3d37bebbaee5b6ffc2fb83
Everton <evertonfpaula@yahoo.com.br>
2023-03-28 14:51:07 -0300

    Alterado cor do botão

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -3,13 +3,13 @@ import './styles.css';
 const Contato = () => {
     return(
         <div className='contato'>
-            <h3>Fale com a gente:</h3>
+            <h3>Fale conosco</h3>
             <form>
                 <input placeholder="Nome Completo"></input>
                 <input placeholder="Email"></input>
                 <input placeholder="Telefone"></input>
                 <textarea placeholder="Mensagem"></textarea>
-                <input type="submit" className='btn-custom'></input>
+                <input type="submit" className='btn-custom' value="Enviar" ></input>
             </form>
         </div>
     );
