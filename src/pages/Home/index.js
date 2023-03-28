db415ec37196b5eb86cdfc8c0c0060934be913ab
Everton <evertonfpaula@yahoo.com.br>
2023-03-28 16:26:20 -0300

    Correção 5g

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,7 @@ const Home = () => {
         <main className='main-home'>
             <img alt='Imagem' src={imagem5g} />
             <div>
-                <h1>Conheça alguns de nossos aparelhos com tecnologia 5Gd</h1>
+                <h1>Conheça alguns de nossos aparelhos com tecnologia 5G</h1>
                 <p>A tecnologia 5G proporciona muitas vantagens, como: conexões ainda mais rápidas para navegar na internet, fazer downloads, baixa latência que garantirá uma comunicação em tempo real.</p>
                 <Link to='/produtos'><button>Confira nossos produtos!</button></Link>
             </div>
